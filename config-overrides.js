7636b5a65a32b243dddf2b645e74bbbc6c938da1
prinzana <prinzana@gmail.com>
2024-12-17 07:06:21 +0100

    bugs wanted to wounf me here, stayed all nigth trying to figure it out

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,24 @@
-module.exports = function override(config, env) {
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: (config, env) => {
+    // Add polyfills for missing Node.js core modules
     config.resolve.fallback = {
+      ...config.resolve.fallback,
+      stream: require.resolve('stream-browserify'),
       crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer'),
+      // Add other modules as needed
     };
+
+    // Provide Buffer globally in the browser
+    config.plugins = [
+      ...config.plugins,
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'], // Make Buffer available globally
+      }),
+    ];
+
     return config;
-  };
-  
+  },
+};
